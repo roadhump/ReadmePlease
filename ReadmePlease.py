33863b76b2d2e0a73d225aff1c1f7ac119526d4d
Aliaksei Shytkin <e79eas@gmail.com>
2014-05-17 10:56:16 +0300

    No inner packages README, flake code check

--- ReadmePlease.py
+++ ReadmePlease.py
@@ -1,38 +1,38 @@
-import sublime, sublime_plugin
-import os
-import re
+import sublime
+import sublime_plugin
+
 
 class ReadmePleaseCommand(sublime_plugin.WindowCommand):
 
-  def description(self):
-    'Quick access to packages README'
+    def description(self):
+        """Quick access to packages README."""
 
-  def run(self):
+    def run(self):
 
-    variations = ["readme.*"]
+        variations = ["readme.*"]
 
-    self.helps = []
+        self.helps = []
 
-    for spelling in variations:
-        readmes = sublime.find_resources(spelling)
+        for spelling in variations:
+            readmes = sublime.find_resources(spelling)
 
-        for readme in readmes:
+            for readme in readmes:
 
-            package = (str(readme).split('/'))
-            plength = (len(package))
+                package = (str(readme).split('/'))
+                plength = (len(package))
 
-            # weeding out the readme files from package libraries.
-            if plength < 7:
-                package_name = package[plength - 2]
-                readme_name = package[plength - 1]
-                self.helps.append([package_name, readme_name, readme])
+                if plength == 3:
+                    package_name = package[plength - 2]
+                    readme_name = package[plength - 1]
+                    self.helps.append([package_name, readme_name, readme])
 
+        self.helps.sort()
+        self.window.show_quick_panel(list(map(
+            lambda x: [x[0], x[1]], self.helps)), self.onSelect)
 
-    self.helps.sort()
-    self.window.show_quick_panel(list(map(lambda x: [x[0], x[1]], self.helps)), self.onSelect)
+    def onSelect(self, i):
+        if (i != -1):
+            help = self.helps[i]
 
-  def onSelect(self, i):
-    if (i != -1):
-        help = self.helps[i]
-
-        help_view = sublime.active_window().run_command("open_file", { "file": "${packages}/%s/%s" % (help[0],help[1])})
+            sublime.active_window().run_command("open_file", {
+                "file": "${packages}/%s/%s" % (help[0], help[1])})
