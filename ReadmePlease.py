a764aedc58cee47fed68df8a79fcbfc8139c9c1d
til <tilman.boerner@gmx.net>
2014-11-03 14:50:09 +0100

    Remove ST2 compatibility fallback
    Plugin will now break on ST2, because sublime.find_resources is not
    defined there.
    
    Also, go back to original way of splitting paths by '/' instead
    of os.path.sep, since it apparently worked, while the change was not
    tested on Windows.

--- ReadmePlease.py
+++ ReadmePlease.py
@@ -1,7 +1,6 @@
 import sublime
 import sublime_plugin
 
-import os
 import sys
 
 # On Windows and OSX, with (commonly) case-insensitive file systems,
@@ -10,22 +9,6 @@ if sys.platform in ('win32', 'darwin'):
     README_PATTERNS = ['readme.*']
 else:
     README_PATTERNS = ["readme.*", "README.*", "Readme.*", "ReadMe.*"]
-
-
-try:
-    find_resources = sublime.find_resources
-except AttributeError:
-    import glob
-    def find_resources(pattern):
-        """ Fallback for ST2, where sublime.find_resources does not exist;
-            unlike the original, this function does only perform a NON-RECURSIVE
-            lookup inside sublime.packages_path(), which is all that's needed.
-        """
-        packages_path = sublime.packages_path()
-        base_path = os.path.dirname(packages_path)
-        path_pattern = os.path.join(packages_path, '*', pattern)
-        paths = glob.glob(path_pattern)
-        return (os.path.relpath(p, start=base_path) for p in paths)
 
 
 class ReadmePleaseCommand(sublime_plugin.WindowCommand):
@@ -38,8 +21,8 @@ class ReadmePleaseCommand(sublime_plugin.WindowCommand):
         self.helps = []
 
         for spelling in README_PATTERNS:
-            for path in find_resources(spelling):
-                components = path.split(os.path.sep)
+            for path in sublime.find_resources(spelling):
+                components = path.split('/')
                 if len(components) == 3:      # exclude files in package subdirs
                     package_name, readme_name = components[-2:]
                     self.helps.append([package_name, readme_name, path])
